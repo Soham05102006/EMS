f01f1c7a79e413ca9c6f0c3ce266a6cd2c1c3711
snehaghatole7 <snehaghatole7@gmail.com>
2026-06-07 00:47:01 +0530

    DashBoard Completed

--- EMS/src/pages/Dashboard.jsx
+++ EMS/src/pages/Dashboard.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { dummyEmployeeDashboardData } from '../assets/assets'
+import Loading from '../components/Loading'
+import EmployeeDashboard from '../components/EmloyeeDashboard'
+import AdminDashboard from '../components/AdminDashboard'
+import { dummyAdminDashboardData } from '../assets/assets'
+
+
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+  const [data, setData] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+
+
+  useEffect(() => {
+          setData(dummyAdminDashboardData);
+          setTimeout(() => {
+          setLoading(false);
+      }, 1000)
+  },[]);
+
+  if (loading) return <Loading />
+  if (!data) return <p className="text-center text-slate-500 py-12">Failed to Load data...</p>
+ 
+  if (data.role === "ADMIN"){
+    return (
+      <AdminDashboard data={data} />
+    )
+  } else {
+    return (
+      <EmployeeDashboard data={data} />
+    )
+  }
+
+
+  
 }
 
 export default Dashboard
